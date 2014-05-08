6def283234b514cdd0adead52a1de32f7ee0cfd4
Mikkel Gravgaard <mikkel@klokke.dk>
2014-05-08 22:40:25 +0200

    initial

---
+++ moody-server/src/moody_server/core.clj
@@ -0,0 +1,35 @@
+(ns moody-server.core
+  (:require [ring.middleware.json :refer :all]
+            [ring.adapter.jetty :as jetty]
+            [ring.util.response :refer :all]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [compojure.handler :as handler]))
+
+(defn keywordify [obj]
+  (let [keys (->> (keys obj)
+                  (map keyword))]
+    (zipmap keys (vals obj))))
+
+(def moods (atom []))
+
+(defn add-mood! [mood]
+  (swap! moods conj (keywordify mood)))
+
+(defn get-moods []
+  @moods)
+
+(defroutes app-routes
+;  (GET "/test" [] "<h1>test</h1>")
+  (GET "/moods" []
+       (response @moods))
+  (POST "/moods" {moods :body}
+        (map add-mood! moods)))
+
+(def app
+  (-> (handler/site app-routes)
+      wrap-json-body
+      wrap-json-response))
+
+(defonce server (jetty/run-jetty #'app
+                           {:port 3000 :join? false}))
---
+++ moody-server/test/moody_server/core_test.clj
@@ -0,0 +1,7 @@
+(ns moody-server.core-test
+  (:require [clojure.test :refer :all]
+            [moody-server.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
