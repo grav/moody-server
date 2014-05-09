637897211e22f37c2b2ae5542c24bbc120afac54
Mikkel Gravgaard <mikkel@klokke.dk>
2014-05-09 11:00:56 +0200

    views

--- moody-client/moody/moody/Visualization/MOOVizViewController.m
+++ moody-client/moody/moody/Visualization/MOOVizViewController.m
@@ -4,10 +4,40 @@
 //
 
 #import "MOOVizViewController.h"
+#import <MapKit/MapKit.h>
 
+@interface MOOVizViewController ()<MKMapViewDelegate>
+
+@end
 
 @implementation MOOVizViewController {
 
 }
+
+- (void)loadView {
+    [super loadView];
+    MKMapView *mapView = [MKMapView new];
+    mapView.mapType = MKMapTypeHybrid;
+
+    UISlider *slider = [UISlider new];
+    [@[mapView, slider] enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        [self.view addSubview:obj];
+    }];
+
+    [mapView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(mapView.superview);
+        make.left.equalTo(mapView.superview);
+        make.right.equalTo(mapView.superview);
+        make.bottom.equalTo(slider.mas_top);
+    }];
+
+    [slider mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(slider.superview);
+        make.right.equalTo(slider.superview);
+        make.bottom.equalTo(slider.superview);
+    }];
+
+}
+
 
 @end
