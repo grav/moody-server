f5fdbffe25220de6717a01111e4d0e1be85a9073
Mikkel Gravgaard <mikkel@klokke.dk>
2014-05-09 10:28:57 +0200

    initial client

---
+++ moody-client/moody/moody/MOOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MOOAppDelegate.h
+//  moody
+//
+//  Created by Mikkel Gravgaard on 09/05/14.
+//  Copyright (c) 2014 Betafunk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MOOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ moody-client/moody/moody/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  moody
+//
+//  Created by Mikkel Gravgaard on 09/05/14.
+//  Copyright (c) 2014 Betafunk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MOOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MOOAppDelegate class]));
+    }
+
+}
