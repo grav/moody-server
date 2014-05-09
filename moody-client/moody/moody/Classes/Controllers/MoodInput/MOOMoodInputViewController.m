244d610a94f9a830fde93cf0d27764159317a085
Søren Ulrikkeholm <me@sore.nu>
2014-05-09 19:27:45 +0200

    Polished animation

--- moody-client/moody/moody/Classes/Controllers/MoodInput/MOOMoodInputViewController.m
+++ moody-client/moody/moody/Classes/Controllers/MoodInput/MOOMoodInputViewController.m
@@ -43,16 +43,16 @@
 
     RAC(self.view, backgroundColor) = RACObserve(self, viewModel.backgroundColor);
     RAC(self.mainView.moodImageView, image) = [RACObserve(self, viewModel.moodImage) distinctUntilChanged];
-
     [self rac_liftSelector:@selector(bounceMoodImage:) withSignals:RACObserve(self.mainView.moodImageView, image), nil];
 }
 
 - (void)bounceMoodImage:(id)_ {
+    CGSize size = self.mainView.moodImageView.image.size;
     POPSpringAnimation *anim = [POPSpringAnimation animationWithPropertyNamed:kPOPViewSize];
     anim.springBounciness = 20;
     anim.springSpeed = 20;
-    anim.fromValue = [NSValue valueWithCGSize:CGSizeZero];
-    anim.toValue = [NSValue valueWithCGSize:self.mainView.moodImageView.image.size];
+    anim.fromValue = [NSValue valueWithCGSize:(CGSize){size.width-10.f, size.height-10.f}];
+    anim.toValue = [NSValue valueWithCGSize:size];
     [self.mainView.moodImageView pop_addAnimation:anim forKey:@"bounce"];
 }
 
