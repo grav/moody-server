17825185efae2065a65a436736c45e17e5a55fef
Mikkel Gravgaard <mikkel@klokke.dk>
2014-05-09 10:43:15 +0200

    start on visualization

---
+++ moody-client/moody/moody/Visualization/MOOVizViewController.h
@@ -0,0 +1,13 @@
+//
+// Created by Mikkel Gravgaard on 09/05/14.
+// Copyright (c) 2014 Betafunk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface MOOVizViewController : UIViewController
+
+
+
+@end
---
+++ moody-client/moody/moody/Visualization/MOOVizViewController.m
@@ -0,0 +1,13 @@
+//
+// Created by Mikkel Gravgaard on 09/05/14.
+// Copyright (c) 2014 Betafunk. All rights reserved.
+//
+
+#import "MOOVizViewController.h"
+
+
+@implementation MOOVizViewController {
+
+}
+
+@end
