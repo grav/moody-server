467e8491cbabb8dc0dfd1cd4d8e5e83a641e61e1
Mikkel Gravgaard <mikkel@klokke.dk>
2014-05-10 10:26:35 +0200

    formatting

--- moody-client/moody/moody/NSURLConnection+MOOAdditions.m
+++ moody-client/moody/moody/NSURLConnection+MOOAdditions.m
@@ -10,32 +10,32 @@
 
 @implementation NSURLConnection (MOOAdditions)
 + (RACSignal *)sendRequest:(NSURLRequest *)request responseClass:(Class)class {
-    return [[NSURLConnection rac_sendAsynchronousRequest:request] map:^id(RACTuple *tuple) {
-        RACTupleUnpack(NSURLResponse*response,NSData *data)= tuple;
+    return [[[NSURLConnection rac_sendAsynchronousRequest:request] map:^id(RACTuple *tuple) {
+        RACTupleUnpack(NSURLResponse *response, NSData *data)= tuple;
         NSError *jsonError;
         id json = [NSJSONSerialization JSONObjectWithData:data
-                                                             options:0
-                                                               error:&jsonError];
+                                                  options:0
+                                                    error:&jsonError];
         NSCAssert(!jsonError, @"%@", jsonError);
         id obj;
 
-        if([json isKindOfClass:[NSArray class]]){
+        if ([json isKindOfClass:[NSArray class]]) {
             obj = [json mapUsingBlock:^id(NSDictionary *jsonDict) {
                 NSError *mtlError;
                 id obj2 = [MTLJSONAdapter modelOfClass:class
-                                           fromJSONDictionary:jsonDict error:&mtlError];
-                NSCAssert(!mtlError, @"%@",mtlError);
+                                    fromJSONDictionary:jsonDict error:&mtlError];
+                NSCAssert(!mtlError, @"%@", mtlError);
                 return obj2;
             }];
         } else {
             NSError *mtlError;
             obj = [MTLJSONAdapter modelOfClass:class
-                               fromJSONDictionary:json error:&mtlError];
-            NSCAssert(!mtlError, @"%@",mtlError);
+                            fromJSONDictionary:json error:&mtlError];
+            NSCAssert(!mtlError, @"%@", mtlError);
         }
 
         return obj;
-    }];
+    }] deliverOn:[RACScheduler mainThreadScheduler]];
 
 }
 
